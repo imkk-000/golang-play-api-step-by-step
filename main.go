72d40ac629997518ec867772a79b4e8172615792
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2019-03-08 22:21:40 +0700

    [Added]: DefaultResponseMessage to APIHandler struct

--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 // APIHandler - implementation of interface Handler
-type APIHandler struct{}
+type APIHandler struct {
+	DefaultResponseMessage string
+}
 
 func (APIHandler APIHandler) ServeHTTP(responseWriter http.ResponseWriter, request *http.Request) {
 	responseWriter.Header().Add("Content-Type", "application/json")
-	responseWriter.Write([]byte("My API!"))
+	responeMessage := fmt.Sprintf(`{"responseMessage":"%s"}`, APIHandler.DefaultResponseMessage)
+	responseWriter.Write([]byte(responeMessage))
 }
 
 func main() {
-	http.Handle("/", APIHandler{})
-	http.ListenAndServe(":8080", nil)
+	log.Fatalln(http.ListenAndServe(":8080", APIHandler{
+		DefaultResponseMessage: "404 Not Found",
+	}))
 }
