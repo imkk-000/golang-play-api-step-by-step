ed9ea23d5f634d5dab933be2c1a2f3ed493bc0d4
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2019-03-08 22:10:58 +0700

    [Edited]: move ramda function to new function name: APIHandler

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import "net/http"
 
+func APIHandler(responseWriter http.ResponseWriter, request *http.Request) {
+	responseWriter.Header().Add("Content-Type", "application/json")
+	responseWriter.Write([]byte("My API!"))
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-Type", "application/json")
-		w.Write([]byte("My API!"))
-	})
+	http.HandleFunc("/", APIHandler)
 	http.ListenAndServe(":8080", nil)
 }
